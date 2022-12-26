5833319493c7d04efb1566692ae7de639d18a872
LaetitiaHenry <115590410+LaetitiaHenry@users.noreply.github.com>
2022-12-26 15:00:53 +0100

    Update Exercice_2.py

--- Exercice_2.py
+++ Exercice_2.py
@@ -1,32 +1,44 @@
+#Exercice n° 2:
+#Rédiger un script permettant d'afficher les statistiques globales à l'échelle du groupe
+
+#Importer les modules
 import sqlite3
 import csv
 
-#connect to the database
+#Connexion à la base de données
 connect = sqlite3.connect('/Users/jfoster/Documents/PluriTAL/BDD/test/bars.db')
 cursor = connect.cursor()
 
-#Les statistiques globale du groupe
+#Trouver le nombre total de bars
+#Compter simplement le nombre de lignes pour la colonne où il y a les noms de la table établissements
 nombre_bars = cursor.execute("SELECT COUNT (name) FROM etablissements")
 res1 = cursor.fetchall()
 for item in res1:
     print(f"Nombre de Bars = {item}")
 
-#Donc le nombre d'employés sans les managers inclus 
-nombre_employes = cursor.execute("SELECT COUNT (matricule) FROM employes WHERE profession != 'Manager' ")
+#Trouver le nombre d'employés 
+#Compter simplement le nombre de lignes pour la colone matricule de la table employés
+nombre_employes = cursor.execute("SELECT COUNT (matricule) FROM employes")
 res2 = cursor.fetchall()
 for item in res2:
     print(f"Nombre d'Employés = {item}")
 
+#Trouver le nombre total de managers
+#Compter le nombre de ligne où la profession est "manager"  
 managers = cursor.execute("SELECT COUNT (matricule) FROM employes WHERE profession = 'Manager' ")
 res3 = cursor.fetchall()
 for item in res3:
     print(f"Nombre de Managers = {item}")
 
+#Trouver le nombre d'employés par profession
+#Compter le nombre de lignes pour chaque type de profession en utilisant "GROUP BY"
 emp_par_profession = cursor.execute("SELECT profession, COUNT (matricule) FROM employes GROUP BY profession")
 res4= cursor.fetchall()
 for item in res4:
     print(item)
 
+#Trouver le revenu total 
+#Additionner le prix de toutes les boissons vendues 
 revenu_groupe = cursor.execute (""" SELECT SUM(S) FROM
                                               (SELECT COUNT(boisson_id) * prix AS S
                                               FROM ventes, carte
@@ -37,7 +49,7 @@ revenu_groupe = cursor.execute (""" SELECT SUM(S) FROM
 res5 = cursor.fetchall()
 for item in res5:
     print(f"Revenu du Groupe = {item}")
-    
+  
+#Sauvegarde et déconnexion de la base 
 connect.commit()
-
 connect.close()
